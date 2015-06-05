f576b62b3720d0146c35487e6970c335c9f6ec7d
alim459 <alim459@gmail.com>
2015-06-05 00:50:05 -0700

    working up to task 6

---
+++ rango/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, url
+from rango import views
+from django.conf import settings
+
+urlpatterns = patterns('',
+        url(r'^$', views.index, name='index'))
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'^media/(?P<path>.*)',
+        'serve',
+        {'document_root': settings.MEDIA_ROOT}), )
